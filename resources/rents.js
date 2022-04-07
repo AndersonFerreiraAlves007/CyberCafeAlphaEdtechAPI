339e51ed0540814871ce0cf13fc7b6267233684d
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2022-04-06 21:10:45 -0300

    feat(resource rents): Estrutura primária das rotas para rents

--- resources/rents.js
+++ resources/rents.js
@@ -1,70 +1,76 @@
 const express = require('express')
 const { createClientPG, destroyClientPG } = require('../utils/clientePG')
+const { formatTimestamps } = require('../utils/date')
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
   const client = await createClientPG()
-  // querys
+  const text = 'SELECT * FROM public.rents'
+  const values = []
+  const result = await client.query(text, values)
   await destroyClientPG(client)
-  res.json({
-    // resultado das querys aqui!
-  })
+  res.json(result.rows)
 })
 
 router.get('/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10)
   const client = await createClientPG()
-  // querys
+  const text = 'SELECT * FROM public.rents Where id=$1'
+  const values = [id]
+  const result = await client.query(text, values)
   await destroyClientPG(client)
-  res.json({
-    // resultado das querys aqui!
-  })
+  res.json(result.rows[0])
 })
 
 router.post('/', async (req, res) => {
+  const { clientFgk, roomsFgk, value, join, leave } = req.body
   const client = await createClientPG()
   try {
-    // querys
+    const text = 'INSERT INTO public.rents (client_fgk, rooms_fgk, value, join, leave) VALUES ($1, $2, $3, $4, $5) RETURNING *'
+    const values = [clientFgk, roomsFgk, value, formatTimestamps(new Date(join)), formatTimestamps(new Date(leave))]
+    const result = await client.query(text, values)
     await destroyClientPG(client)
-    res.json({
-      // resultado das querys aqui!
-    })
+    res.json(result.rows[0])
   } catch (e) {
     await destroyClientPG(client)
     res.status(400).json({
-      message: 'Mensagem de erro aqui!'
+      message: e
     })
   }
 })
 
 router.put('/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10)
+  const { clientFgk, roomsFgk, value, join, leave } = req.body
   const client = await createClientPG()
   try {
-    // querys
+    const text = 'UPDATE public.rents SET update_in=$1, update_by=$2, client_fgk=$3, rooms_fgk=$4, value=$5, join=$6, leave=$7 WHERE id=$8 RETURNING *'
+    const values = [formatTimestamps(new Date()), 1, clientFgk, roomsFgk, value, formatTimestamps(new Date(join)), formatTimestamps(new Date(leave)), id]
+    const result = await client.query(text, values)
     await destroyClientPG(client)
-    res.json({
-      // resultado das querys aqui!
-    })
+    res.json(result.rows[0])
   } catch (e) {
     await destroyClientPG(client)
     res.status(400).json({
-      message: 'Mensagem de erro aqui!'
+      message: e
     })
   }
 })
 
 router.delete('/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10)
   const client = await createClientPG()
   try {
-    // querys
+    const text = 'UPDATE public.rents SET delete_in=$1, delete_by=$2 WHERE id=$3 RETURNING *'
+    const values = [formatTimestamps(new Date()), 1, id]
+    const result = await client.query(text, values)
     await destroyClientPG(client)
-    res.json({
-      // resultado das querys aqui!
-    })
+    res.json(result.rows[0])
   } catch (e) {
     await destroyClientPG(client)
     res.status(400).json({
-      message: 'Mensagem de erro aqui!'
+      message: e
     })
   }
 })
---
+++ utils/date.js
@@ -0,0 +1,23 @@
+function pad (number, length) {
+  let str = '' + number
+  while (str.length < length) {
+    str = '0' + str
+  }
+
+  return str
+}
+
+function formatTimestamps (date) {
+  const dt = date
+  const dtstring = dt.getFullYear() +
+      '-' + pad(dt.getMonth() + 1, 2) +
+      '-' + pad(dt.getDate(), 2) +
+      ' ' + pad(dt.getHours(), 2) +
+      ':' + pad(dt.getMinutes(), 2) +
+      ':' + pad(dt.getSeconds(), 2)
+  return dtstring
+}
+
+module.exports = {
+  formatTimestamps
+}
