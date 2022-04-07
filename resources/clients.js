baf91867b2bc23277b79e0ae91c354410489c4ef
Felipe Bueno <felipebuenonunes16@gmail.com>
2022-04-07 17:29:12 -0300

    update clients

--- resources/clients.js
+++ resources/clients.js
@@ -4,31 +4,31 @@ const { createClientPG, destroyClientPG } = require('../utils/clientePG')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const client = await createClientPG()
-  // querys
-  await destroyClientPG(client)
-  res.json({
-    // resultado das querys aqui!
-  })
-})
+  const client = await createClientPG();
+  const text = 'SELECT * FROM public.clients';
+  const result = await client.query(text);
+  await destroyClientPG(client);
+  res.json(result.rows);
+});
 
 router.get('/:id', async (req, res) => {
-  const client = await createClientPG()
-  // querys
-  await destroyClientPG(client)
-  res.json({
-    // resultado das querys aqui!
-  })
-})
+  const id = parseInt(req.params.id, 10);
+  const client = await createClientPG();
+  const text = 'SELECT * FROM public.clients Where id=$1';
+  const result = await client.query(text, [id]);
+  await destroyClientPG(client);
+  res.json(result.rows[0]);
+});
 
 router.post('/', async (req, res) => {
-  const client = await createClientPG()
+  const { name, email, cpf, username, create_by } = req.query;
+  const client = await createClientPG();
   try {
-    // querys
-    await destroyClientPG(client)
-    res.json({
-      // resultado das querys aqui!
-    })
+    const text = "INSERT INTO public.clients (name, username, email, cpf, create_by, create_in) VALUES ($1, $2, $3, $4, $5);";
+    const values = [name, username, email, cpf, create_by, formatTimestamps(new Date(join))];
+    const result = await client.query(text, values);
+    await destroyClientPG(client);
+    res.json(result.rows[0]);
   } catch (e) {
     await destroyClientPG(client)
     res.status(400).json({
@@ -56,13 +56,16 @@ router.put('/:id', async (req, res) => {
 })
 
 router.delete('/:id', async (req, res) => {
-  const client = await createClientPG()
+  const id = req.params.id;
+  const deleteBy = req.body.delete_by;
+  const client = await createClientPG();
   try {
     // querys
+    const text = 'UPDATE public.rents SET delete_in=$1, delete_by=$2 WHERE id=$3';
+    const values = [formatTimestamps(new Date()), deleteBy, id];
+    const result = await client.query(text, values);
     await destroyClientPG(client)
-    res.json({
-      // resultado das querys aqui!
-    })
+    res.json(result);
   } catch (e) {
     await destroyClientPG(client)
     res.status(400).json({
