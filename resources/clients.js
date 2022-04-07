917ad594b8925dfc9ebbdba7c22c3231993aced4
Fabrício <fabri.rp2010@gmail.com>
2022-04-07 08:14:39 -0300

    update client

--- resources/clients.js
+++ resources/clients.js
@@ -38,18 +38,20 @@ router.post('/', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const { name, email, cpf, username, user_fgk} = req.body
   const client = await createClientPG()
   try {
-    // querys
-    await destroyClientPG(client)
-    res.json({
-      // resultado das querys aqui!
-    })
+    const text = 'UPDATE public.clients SET update_in=$1, update_by=$2, name=$3, email=$4, cpf=$5, username=$6 WHERE id=$8 RETURNING *';
+    const values = [formatTimestamps(new Date()), user_fgk, name, email, cpf, username, id];
+    const result = await client.query(text, values);
+    await destroyClientPG(client);
+    res.json(result.rows[0])
   } catch (e) {
     await destroyClientPG(client)
     res.status(400).json({
-      message: 'Mensagem de erro aqui!'
-    })
+      message: e
+    });
   }
 })
 
