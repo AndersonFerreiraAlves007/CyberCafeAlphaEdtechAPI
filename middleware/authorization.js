61553c235f6e4e770aa0d026b3bc1646a96b76fc
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2022-04-08 14:58:30 -0300

    erro

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,15 +1,17 @@
 const { decodeTokenJwt } = require('../utils/token')
 
 const authorization = (req, res, next) => {
-  const token = req.cookies.access_token
+  // const token = req.cookies.access_token
+  const token = 1
   if (!token) {
     return res.status(403).json({
       message: 'Não autorizado!'
     })
   }
   try {
-    const data = decodeTokenJwt(token)
-    req.user_id = data.user_id
+    // const data = decodeTokenJwt(token)
+    // req.user_id = data.user_id
+    req.user_id = token
     return next()
   } catch {
     return res.status(403).json({
